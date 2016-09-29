28a6abf601a92608af0fd246c046bd7f5d745e28
Bert Spaan <bert.spaan@gmail.com>
2016-09-29 15:22:13 -0400

    added addCollections function

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ module.exports.addCollection = (collection, callback) => {
   db.replaceRows('collections', collection.provider, collection.id, [collection], callback)
 }
 
+module.exports.addCollections = (collections, callback) => {
+  if (collections && collections.length) {
+    const provider = collections[0].provider
+    const collectionId = collections[0].id
+    db.replaceRows('collections', provider, collectionId, collections, callback)
+  } else {
+    callback()
+  }
+}
+
 module.exports.addItems = (items, callback) => {
   if (items && items.length) {
     const provider = items[0].provider
